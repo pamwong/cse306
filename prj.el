461217499c57ce903aed17a3d8520f25f8716891
stark <>
2003-01-21 19:50:50 +0000

    Add this project file for Emacs/JDE.

---
+++ prj.el
@@ -0,0 +1,8 @@
+(jde-project-file-version "1.0")
+(jde-set-variables
+ '(jde-make-args "all")
+ '(jde-make-working-directory "~/nachos/code/threads/")
+ '(jde-build-function (quote (jde-make)))
+ '(jde-global-classpath (quote ("~/nachos/code/machine" "~/nachos/code/filesys" "~/nachos/code/threads" "~/nachos/code/userprog" "~/nachos/code/vm")))
+ '(jde-run-application-class "Nachos")
+ '(jde-make-program "make"))
