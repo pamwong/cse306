fb1da27e92005c7087f9593df35ce530a25a789b
stark <>
2003-08-12 17:34:48 +0000

    Update the prj.el to match what it says in my HW0 handout.

--- prj.el
+++ prj.el
@@ -1,9 +1,9 @@
 (jde-project-file-version "1.0")
 (jde-set-variables
  '(jde-make-args "all")
- '(jde-make-working-directory "~/306/Work/nachos.new")
+ '(jde-make-working-directory "~/nachos")
  '(jde-build-function (quote (jde-make)))
- '(jde-global-classpath (quote ("~/306/Work/nachos.new/machine.jar"
-				"~/306/Work/nachos.new")))
+ '(jde-global-classpath (quote ("~/nachos/machine.jar"
+				"~/nachos")))
  '(jde-run-application-class "nachos.kernel.Nachos")
  '(jde-make-program "make"))
